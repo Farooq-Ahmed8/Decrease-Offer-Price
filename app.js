1392b83b4649e68bcc6221ed71f309e1b716a447
Farooq Ahmed <03174748082ali@gmail.com>
2025-07-28 09:09:19 -0700

    work completed

---
+++ app.js
@@ -0,0 +1,10 @@
+document.write("<h1>", "Before Offer","<h1/>")
+let price = [300, 600, 200, 450, 850];
+document.write("<h1>", price,"<h1/>")
+for(let i = 0; i < price.length; i++){
+    let offer = price[i] / 10;
+     price[i] -= offer
+    
+}
+document.write("<h1>","After Offer","<h1/>")
+document.write("<h1>", price,"<h1/>")
