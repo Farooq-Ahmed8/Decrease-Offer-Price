47fd7d87681e0f523b212ca006b9d1fde3af6fea
Farooq Ahmed <03174748082ali@gmail.com>
2025-07-28 23:40:06 -0700

    completed

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-document.write("<h1>", "Before Offer","<h1/>")
+document.write("<h1>", "Before Offer","<h1/>");
 
 let price = [300, 600, 200, 450, 850];
 
-document.write("<h1>", price,"<h1/>")
+document.write("<h1>", price,"<h1/>");
 
 for(let i = 0; i < price.length; i++){
     let offer = price[i] / 10;
-     price[i] -= offer
+     price[i] -= offer;
     
 }
-document.write("<h1>","After Offer","<h1/>")
-document.write("<h1>", price,"<h1/>")
+document.write("<h1>","After Offer","<h1/>");
+document.write("<h1>", price,"<h1/>");
